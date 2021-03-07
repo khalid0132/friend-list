e640138f6512bf22fb778d2c05b5ef2ae3e3fedf
Khalid Saifullah <khalid0132@gmail.com>
2021-03-08 00:05:53 +0100

    style added

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const NoMatch = () => {
+    const noMatch = {
+        textAlign: 'center',
+        border: '1px solid gray',
+        boxShadow: '5px 5px 10px gray',
+        padding: '20px',
+        width: '400px',
+        margin: '0 auto',
+        marginTop: '50px',
+        borderRadius: '20px'
+    }
 
     return (
-        <div>
+        <div style={noMatch}>
             <h2>Page not found</h2>
             <h1>Error, Try again.....</h1>
         </div>
